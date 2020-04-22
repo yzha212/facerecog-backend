2651fc2f5a130102fcc742b1a857108444d2ff54
Yang Zhang <yza457@sfu.ca>
2020-04-22 11:41:41 -0700

    modified connection to database

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controller/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : process.env.DATABASE_URL,
+    host: process.env.DATABASE_URL,
     ssl:true
   }
 });
