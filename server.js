b93b528cad22141f0e8ef2cad1e34f23835d2747
Yang Zhang <yza457@sfu.ca>
2020-04-22 11:41:22 -0700

    modified connection to db

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controller/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'postgres',
-    database : 'facerecog'
+    host : process.env.DATABASE_URL,
+    ssl:true
   }
 });
 
