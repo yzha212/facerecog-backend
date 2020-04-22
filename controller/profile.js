bd2205fcbf04bee3da6e9ce2e3c5d7920845e167
Yang Zhang <yza457@sfu.ca>
2020-04-21 19:42:54 -0700

    push backend to git

---
+++ controller/profile.js
@@ -0,0 +1,16 @@
+const handleProfileGet = (db) => (req, res) => {
+  const { id } = req.params;
+  db.select('*').from('users').where({id})
+    .then(user => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(400).json('not found');
+      }
+  })
+  .catch(err => res.status(400).json('not found'))
+}
+
+module.exports = {
+  handleProfileGet: handleProfileGet
+}
---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controller/register');
+const signin = require('./controller/signin');
+const profile = require('./controller/profile');
+const image = require('./controller/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'postgres',
+    database : 'facerecog'
+  }
+});
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send(database.users);
+});
+
+app.post('/signin', signin.handleSignin(db, bcrypt));
+app.post('/register', register.handleRegister(db, bcrypt)); // this is dependency injection
+app.get('/profile/:id', profile.handleProfileGet(db));
+app.put('/image', image.handleImage(db));
+app.post('/imageurl', image.handleApiCall);
+
+app.listen(3000, () => {
+  console.log('app is running on port 3000');
+});
+
+/*
+/                 --> res = this is working
+/signin           --> POST = success or fail
+                      send password in body, not via query string
+/register         --> POST = return user object
+/profile/:userID  --> GET = return user; userID is param
+/image            --> PUT = return updated user object (e.g. count)
+
+*/
