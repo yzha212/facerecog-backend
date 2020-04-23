434d924b7b0e68f903c1fb868a911e1468cd8ff3
Yang Zhang <yza457@sfu.ca>
2020-04-23 11:30:17 -0700

    bug fix

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send(database.users);
+  res.send(db.users);
 });
 
 app.post('/signin', signin.handleSignin(db, bcrypt));
